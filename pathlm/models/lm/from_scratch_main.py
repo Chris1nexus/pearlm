e0d6ec9f6542d91dbe63b3ef29bf1e14525847e5
chris1nexus <chris.2nexus@gmail.com>
2023-05-19 18:19:19 +0200

    Fix bugs

--- pathlm/models/lm/from_scratch_main.py
+++ pathlm/models/lm/from_scratch_main.py
@@ -19,6 +19,7 @@ from tokenizers import (
     Tokenizer,
 )
 
+
 WORD_LEVEL_TOKENIZER = "./tokenizers/ml1m/WordLevel.json"
 # Read an example and return the tokenized version
 def tokenize_function(examples: str):
@@ -41,7 +42,7 @@ def group_texts(examples: List[str], block_size=256):
     result["labels"] = result["input_ids"].copy()
     return result
 
-def train_from_scratch(model_name: str, args: argparse.Namespace):
+def train_from_scratch(model_name: str, tokenizer, tokenized_dataset, context_length, args: argparse.Namespace):
 
     # Initializing the selected model style configuration
     config = AutoConfig.from_pretrained(
@@ -59,17 +60,19 @@ def train_from_scratch(model_name: str, args: argparse.Namespace):
     # Training arguments
     custom_name = f"from_scratch-{args.data}-{args.model}"
 
+    BATCH_SIZE = 64
     # Training arguments for Causal Language Model task
     training_args = TrainingArguments(
         f"clm-{custom_name}",
-        evaluation_strategy="steps",
-        eval_steps=1000,
+            evaluation_strategy="epoch",
+            save_strategy='epoch',
+        eval_steps=100000,
         learning_rate=2e-5,
         weight_decay=0.01,
-        use_mps_device=True,
-        num_train_epochs=25,
-        per_device_train_batch_size=8,
-        per_device_eval_batch_size=8,
+        #use_mps_device=True,
+        num_train_epochs=1,
+        per_device_train_batch_size=BATCH_SIZE,
+        per_device_eval_batch_size=BATCH_SIZE,
         warmup_steps=1000,  # number of warmup steps for learning rate
         save_steps=1000,
         save_total_limit=2,
@@ -111,27 +114,40 @@ if __name__ == "__main__":
     parser.add_argument("--data", type=str, default="ml1m", help="{ml1m, lfm1m}")
     parser.add_argument("--model", type=str, default="distilgpt2", help="Model to use from HuggingFace pretrained models")
     parser.add_argument("--seed", type=int, default=123, help="Seed for reproducibility")
+    parser.add_argument("--nproc", type=int, default=4, help="Number of processes for dataset mapping")
     parser.add_argument("--load_data", type=bool, default=False, help="")
     parser.add_argument("--load_model", type=bool, default=False, help="")
     args = parser.parse_args()
 
+
     model_name = args.model
     dataset_name = args.data
+
+    tokenizer_dir = f'./tokenizers/{dataset_name}'
+    os.makedirs(tokenizer_dir, exist_ok=True)
+    tokenizer_file = os.path.join(tokenizer_dir, f"WordLevel.json")
+    context_length = 256
+    
+
     # Try to load the dataset from disk if it has been already tokenized otherwise load it from scratch
     if args.load_data:
-        dataset = load_from_disk(f"data/{dataset_name}/{model_name}/from_scratch_tokenized_dataset.hf")
+        tokenized_dataset = load_from_disk(f"data/{dataset_name}/{model_name}/from_scratch_tokenized_dataset.hf")
+        tokenizer = PreTrainedTokenizerFast(tokenizer_file=tokenizer_file , max_len=256,
+                                            eos_token="[EOS]", bos_token="[BOS]",
+                                            pad_token="[PAD]", unk_token="[UNK]",
+                                            mask_token="[MASK]", use_fast=True)        
     else:
         # Load the dataset
         data_dir = f"data/{dataset_name}"
         plain_text_path=False
-        nproc=6
+        
         dataset = PathDataset(dataset_name, data_dir, plain_text_path=plain_text_path)
         if plain_text_path:
             dataset.dataset = dataset.dataset.map(lambda x: {"path": [dataset.convert_numeric_path_to_textual_path(elem) for elem in x["path"] ] },
-                            batched=True, num_proc=nproc)
+                            batched=True, num_proc=args.nproc)
         else:
             dataset.dataset = dataset.dataset.map(lambda x: {"path": [dataset.keep_numeric(elem) for elem in x["path"] ]  }, 
-                                        batched=True, num_proc=nproc)        
+                                        batched=True, num_proc=args.nproc)        
         dataset.show_random_examples()
         dataset = dataset.dataset
 
@@ -145,8 +161,8 @@ if __name__ == "__main__":
             single="[EOS]:0 $A:0 [BOS]:0",
             special_tokens=[("[EOS]", tokenizer.token_to_id("[EOS]")), ("[BOS]", tokenizer.token_to_id("[BOS]"))]
         )
-        os.makedirs(f'./tokenizers/{dataset_name}', exist_ok=True)
-        tokenizer.save(f"./tokenizers/{dataset_name}/WordLevel.json")
+        
+        tokenizer.save(tokenizer_file)
 
         #Check correctness of the encoding
         #print(dataset["path"][0], tokenizer.encode(dataset["path"][0]).tokens)
@@ -155,22 +171,27 @@ if __name__ == "__main__":
         dataset_split = dataset.train_test_split(test_size=0.1)
 
         # Tokenizer and tokenization function
-        context_length = 256
         tokenizer = PreTrainedTokenizerFast(tokenizer_object=tokenizer, max_len=context_length,
                                             eos_token="[EOS]", bos_token="[BOS]",
                                             pad_token="[PAD]", unk_token="[UNK]",
                                             mask_token="[MASK]", use_fast=True)
-        tokenized_dataset = dataset_split.map(tokenize_function, batched=True, num_proc=4, remove_columns=["path"])
+        tokenized_dataset = dataset_split.map(tokenize_function, 
+            batched=True, 
+            num_proc=args.nproc,
+            remove_columns=["path"]
+        )
         # Group texts into chunks of block_size tokens
         tokenized_dataset = tokenized_dataset.map(
             group_texts,
             batched=True,
             batch_size=1000,
-            num_proc=4,
+            num_proc=args.nproc,
         )
         # Create a dir if does not exist for the hf dataset and save the tokenized dataset to disk
         check_dir(f"{data_dir}/{model_name}/from_scratch_tokenized_dataset.hf")
         tokenized_dataset.save_to_disk(f"data/{dataset_name}/{model_name}/from_scratch_tokenized_dataset.hf")
+
+
 
     # Train the model
     if args.load_model:
@@ -178,7 +199,7 @@ if __name__ == "__main__":
         custom_name = f"from_scratch-{args.data}-{args.model}"
         model = AutoModelForCausalLM.from_pretrained(f"models-weights/{dataset_name}/{model_name}/{custom_name}")
     else:
-        model = train_from_scratch(model_name, args)
+        model = train_from_scratch(model_name, tokenizer, tokenized_dataset, context_length, args)
     evaluate(model, args)
 
 
