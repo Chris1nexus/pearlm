f15d84cd5499d477827c353bd2f038ad47ba5c34
giacoballoccu <giacoballoccu@gmail.com>
2023-10-06 14:17:58 +0200

    Added Datasets classes

---
+++ pathlm/datasets/data_utils.py
@@ -0,0 +1,57 @@
+import csv
+from collections import defaultdict
+from typing import Dict, List
+from tqdm import tqdm
+from pathlm.utils import get_dataset_id2eid
+
+
+def get_user_negatives(dataset_name: str) -> Dict[int, List[int]]:
+    """
+    Returns a dictionary with the user negatives in the dataset, this means the items not interacted in the train and valid sets.
+    Note that the ids are the entity ids to be in the same space of the models.
+    """
+    pid2eid = get_dataset_id2eid(dataset_name, what='product')
+    ikg_ids = set(pid2eid.values()) # All the ids of products in the kg
+    uid_negatives = {}
+    # Generate paths for the test set
+    train_set = get_set(dataset_name, set_str='train')
+    valid_set = get_set(dataset_name, set_str='valid')
+    for uid in tqdm(train_set.keys(), desc="Calculating user negatives", colour="green"):
+        uid_negatives[uid] = list(set(ikg_ids - set(train_set[uid]) - set(valid_set[uid])))
+    return uid_negatives
+
+
+def get_set(dataset_name: str, set_str: str = 'test') -> Dict[int, List[int]]:
+    """
+    Returns a dictionary containing the user interactions in the selected set {train, valid, test}.
+    Note that the ids are the entity ids to be in the same space of the models.
+    """
+    data_dir = f"data/{dataset_name}"
+    # Note that test.txt has uid and pid from the original dataset so a convertion from dataset to entity id must be done
+    uid2eid = get_dataset_id2eid(dataset_name, what='user')
+    pid2eid = get_dataset_id2eid(dataset_name, what='product')
+
+    # Generate paths for the test set
+    curr_set = defaultdict(list)
+    with open(f"{data_dir}/preprocessed/{set_str}.txt", "r") as f:
+        reader = csv.reader(f, delimiter="\t")
+        for row in reader:
+            user_id, item_id, rating, timestamp = row
+            user_id = int(uid2eid[user_id])  # user_id starts from 1 in the augmented graph starts from 0
+            item_id = int(pid2eid[item_id])  # Converting dataset id to eid
+            curr_set[user_id].append(item_id)
+    f.close()
+    return curr_set
+
+
+def get_user_positives(dataset_name: str) -> Dict[int, List[int]]:
+    """
+    Returns a dictionary with the user positives in the dataset, this means the items interacted in the train and valid sets.
+    Note that the ids are the entity ids to be in the same space of the models.
+    """
+    uid_positives = {}
+    train_set = get_set(dataset_name, set_str='train')
+    valid_set = get_set(dataset_name, set_str='valid')
+    for uid in tqdm(train_set.keys(), desc="Calculating user negatives", colour="green"):
+        uid_positives[uid] = list(set(train_set[uid]).union(set(valid_set[uid])))
+    return uid_positives
---
+++ pathlm/datasets/kgat_dataset.py
@@ -0,0 +1,241 @@
+import collections
+import numpy as np
+import os
+from torch.utils.data import Dataset
+
+class KGATStyleDataset(Dataset):
+    """
+    This dataset is used by the following models: {BPRMF, FM, NFM, CKE, CFKG, KGAT}
+    """
+    def __init__(self, args, path, batch_style='list'):
+        super(KGATStyleDataset).__init__()
+
+        self.batch_styles = {'list': 0, 'map': 1}
+        assert batch_style in self.batch_styles, f"Error: got {batch_style} but valid batch styles are {list(self.batch_styles.keys())}"
+        self.path = path
+        self.args = args
+        self.batch_style = batch_style
+        self.batch_style_id = self.batch_styles[self.batch_style]
+
+        self.batch_size = args.batch_size
+
+        # Load data
+        self.train_data, self.train_user_dict = self._load_ratings(os.path.join(path, 'train.txt'))
+        self.valid_data, self.valid_user_dict = self._load_ratings(os.path.join(path, 'valid.txt'))
+        self.test_data, self.test_user_dict = self._load_ratings(os.path.join(path, 'test.txt'))
+
+        self.exist_users = list(self.train_user_dict.keys())
+        self.N_exist_users = len(self.exist_users)
+
+        self._statistic_ratings()
+
+        # Load KG data
+        self.kg_data, self.kg_dict, self.relation_dict = self._load_kg(os.path.join(path, 'kg_final.txt'))
+
+        # Print dataset info
+        self.batch_size_kg = self.n_triples // (self.n_train // self.batch_size)
+        self._print_data_info()
+
+        self.layer_size = eval(args.layer_size)[0]
+        self.mess_dropout = eval(args.mess_dropout)[0]
+        self.node_dropout = eval(args.mess_dropout)[0]
+
+    def _load_ratings(self, file_name):
+        user_dict = collections.defaultdict(list)
+        inter_mat = []
+
+        with open(file_name, 'r') as file:
+            for line in file:
+                items = [int(i) for i in line.strip().split()]
+                user, pos_items = items[0], items[1:]
+                inter_mat.extend([(user, item) for item in pos_items])
+                user_dict[user].extend(pos_items)
+
+        return np.array(inter_mat), user_dict
+
+    def _statistic_ratings(self):
+        self.n_users = max(max(self.train_data[:, 0]), max(self.test_data[:, 0])) + 1
+        self.n_items = max(max(self.train_data[:, 1]), max(self.valid_data[:, 1]), max(self.test_data[:, 1])) + 1
+        self.n_train = len(self.train_data)
+        self.n_valid = len(self.valid_data)
+        self.n_test = len(self.test_data)
+
+    def _load_kg(self, file_name):
+        def _construct_kg(kg_np):
+            kg = collections.defaultdict(list)
+            rd = collections.defaultdict(list)
+            for head, relation, tail in kg_np:
+                kg[head].append((tail, relation))
+                rd[relation].append((head, tail))
+            return kg, rd
+
+        kg_np = np.loadtxt(file_name, dtype=np.int32)
+        kg_np = np.unique(kg_np, axis=0)
+
+        self.n_relations = max(kg_np[:, 1]) + 1
+        self.n_entities = max(max(kg_np[:, 0]), max(kg_np[:, 2])) + 1
+        self.n_triples = len(kg_np)
+
+        kg_dict, relation_dict = _construct_kg(kg_np)
+
+        return kg_np, kg_dict, relation_dict
+
+    def _print_data_info(self):
+        print(f'[n_users, n_items]=[{self.n_users}, {self.n_items}]')
+        print(f'[n_train, n_test]=[{self.n_train}, {self.n_test}]')
+        print(f'[n_entities, n_relations, n_triples]=[{self.n_entities}, {self.n_relations}, {self.n_triples}]')
+        print(f'[batch_size, batch_size_kg]=[{self.batch_size}, {self.batch_size_kg}]')
+
+    def get_sparsity_split(self):
+        split_file = os.path.join(self.path, 'sparsity.split')
+        split_uids, split_state = [], []
+
+        try:
+            with open(split_file, 'r') as f:
+                lines = f.readlines()
+
+            for idx, line in enumerate(lines):
+                if idx % 2 == 0:
+                    split_state.append(line.strip())
+                else:
+                    split_uids.append(list(map(int, line.strip().split(' '))))
+            print('Loaded sparsity split.')
+
+        except Exception:
+            split_uids, split_state = self.create_sparsity_split()
+            with open(split_file, 'w') as f:
+                for state, uids in zip(split_state, split_uids):
+                    f.write(state + '\n')
+                    f.write(' '.join(map(str, uids)) + '\n')
+            print('Created sparsity split.')
+
+        return split_uids, split_state
+
+    def create_sparsity_split(self):
+        all_users_to_test = list(self.test_user_dict.keys())
+        user_n_iid = {}
+
+        for uid in all_users_to_test:
+            n_iids = len(self.train_user_dict[uid]) + len(self.test_user_dict[uid])
+            user_n_iid.setdefault(n_iids, []).append(uid)
+
+        split_uids, split_state = [], []
+        temp, n_rates, fold = [], 0, 4
+
+        for n_iids in sorted(user_n_iid):
+            temp.extend(user_n_iid[n_iids])
+            n_rates += n_iids * len(user_n_iid[n_iids])
+
+            if n_rates >= 0.25 * (self.n_train + self.n_test):
+                split_uids.append(temp)
+                state = f'#inter per user<=[{n_iids}], #users=[{len(temp)}], #all rates=[{n_rates}]'
+                split_state.append(state)
+                print(state)
+
+                temp, n_rates = [], 0
+                fold -= 1
+
+            if not temp:
+                continue
+
+            split_uids.append(temp)
+            state = f'#inter per user<=[{n_iids}], #users=[{len(temp)}], #all rates=[{n_rates}]'
+            split_state.append(state)
+            print(state)
+
+        return split_uids, split_state
+
+    def __len__(self):
+        # number of existing users after the preprocessing described in the paper,
+        # determines the length of the training dataset, for which a positive an negative are extracted
+        return len(self.exist_users)
+
+    ##_generate_train_cf_batch
+    def __getitem__(self, idx):
+        """
+        if self.batch_size <= self.n_users:
+            user = rd.sample(self.exist_users, self.batch_size)
+        else:
+            users = [rd.choice(self.exist_users) for _ in range(self.batch_size)]
+        """
+        def sample_pos_items_for_u(u, num):
+            pos_items = self.train_user_dict[u]
+            n_pos_items = len(pos_items)
+            pos_batch = []
+            while True:
+                if len(pos_batch) == num: break
+                pos_id = np.random.randint(low=0, high=n_pos_items, size=1)[0]
+                pos_i_id = pos_items[pos_id]
+
+                if pos_i_id not in pos_batch:
+                    pos_batch.append(pos_i_id)
+            return pos_batch
+
+        def sample_neg_items_for_u(u, num):
+            neg_items = []
+            while True:
+                if len(neg_items) == num: break
+                neg_i_id = np.random.randint(low=0, high=self.n_items,size=1)[0]
+
+                if neg_i_id not in self.train_user_dict[u] and neg_i_id not in neg_items:
+                    neg_items.append(neg_i_id)
+            return neg_items
+        """
+        pos_items, neg_items = [], []
+        for u in users:
+            pos_items += sample_pos_items_for_u(u, 1)
+            neg_items += sample_neg_items_for_u(u, 1)
+        """
+        u = self.exist_users[idx]
+        pos_item = sample_pos_items_for_u(u, 1)
+        neg_item = sample_neg_items_for_u(u, 1)
+        if len(pos_item) == 1:
+            pos_item = pos_item[0]
+        if len(neg_item) == 1:
+            neg_item = neg_item[0]
+
+
+        if self.batch_style_id == 0:
+            return u, pos_item, neg_item
+        else:
+            return {'users': u, 'pos_items': pos_item, 'neg_items':neg_item}#u, pos_item, neg_item #users, pos_items, neg_items
+
+
+    def as_test_feed_dict(self, model, user_batch, item_batch):
+        feed_dict = {
+            model.users: user_batch,
+            model.pos_items: item_batch,
+            model.mess_dropout: [0.] * len(self.layer_size),
+            model.node_dropout: [0.] * len(self.layer_size)
+        }
+        return feed_dict
+
+    def as_train_feed_dict(self, model, batch_data):
+        # Ensure batch_data is in dictionary format
+        if self.batch_style_id == 0:
+            users, pos_items, neg_items = batch_data
+            batch_data = {
+                'users': users,
+                'pos_items': pos_items,
+                'neg_items': neg_items
+            }
+
+        feed_dict = {
+            model.users: batch_data['users'],
+            model.pos_items: batch_data['pos_items'],
+            model.neg_items: batch_data['neg_items'],
+            model.mess_dropout: self.mess_dropout,
+            model.node_dropout: self.node_dropout
+        }
+        return feed_dict
+
+    def prepare_train_data(self, batch_data):
+        feed_dict = {}
+        # Ensure batch_data is in dictionary format
+        if self.batch_style_id == 0:
+            users, pos_items, neg_items = batch_data
+        else:
+            users = batch_data['users']
+            pos_items = batch_data['pos_items']
+            neg_items = batch_data['neg_items']
+        return users, pos_items, neg_items
