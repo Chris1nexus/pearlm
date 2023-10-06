545e5dfef8509d5cbe83baa42823fdbb06dea31f
giacoballoccu <giacoballoccu@gmail.com>
2023-10-06 14:21:16 +0200

    Refactoring pathlm

--- pathlm/evaluation/rec_quality_metrics_old.py
+++ pathlm/evaluation/rec_quality_metrics_old.py
@@ -3,7 +3,7 @@ from utils import *
 from easydict import EasyDict as edict
 
 """
-Implemented metrics
+Implemented evaluation
 """
 NDCG = "ndcg"
 MMR = "mmr"
@@ -20,33 +20,6 @@ REC_QUALITY_METRICS_GLOBAL = [COVERAGE, CFAIRNESS]
 """
 Methods
 """
-
-
-def mmr_at_k(hit_list, k):
-    r = np.asfarray(hit_list)[:k]
-    hit_idxs = np.nonzero(r)
-    if len(hit_idxs[0]) > 0:
-        return 1 / (hit_idxs[0][0] + 1)
-    return 0.
-
-
-def dcg_at_k(hit_list, k, method=1):
-    r = np.asfarray(hit_list)[:k]
-    if r.size:
-        if method == 0:
-            return r[0] + np.sum(r[1:] / np.log2(np.arange(2, r.size + 1)))
-        elif method == 1:
-            return np.sum(r / np.log2(np.arange(2, r.size + 2)))
-        else:
-            raise ValueError('method must be 0 or 1.')
-    return 0.
-
-
-def ndcg_at_k(hit_list, k, method=0):
-    dcg_max = dcg_at_k(sorted(hit_list, reverse=True), k, method)
-    if not dcg_max:
-        return 0.
-    return dcg_at_k(hit_list, k, method) / dcg_max
 
 def print_rec_quality_metrics(avg_metrics, c_fairness):
     print("\n***---Recommandation Quality---***")
@@ -68,40 +41,6 @@ def print_rec_quality_metrics(avg_metrics, c_fairness):
             group_class, avg_value = tuple
             print(f"{metric} Pairwise diff {class_group}: {avg_value:.3f}", end=" | ")
         print("\n")
-
-
-
-
-"""
-Beyond Accuracy
-"""
-
-"""
-    Catalog coverage https://dl.acm.org/doi/pdf/10.1145/2926720
-"""
-
-
-def coverage(recommended_items_by_group, n_items_in_catalog):
-    group_metric_value = {}
-    for group, item_set in recommended_items_by_group.items():
-        group_metric_value[group] = len(item_set) / n_items_in_catalog
-    return group_metric_value
-
-
-def serendipity_at_k(user_topk, most_pop_topk, k):
-    user_topk, most_pop_topk = set(user_topk), set(most_pop_topk)
-    intersection = user_topk.intersection(most_pop_topk)
-    return (k-len(intersection)) / k
-
-
-def diversity_at_k(topk_items, pid2genre):
-    diversity_items_tok = set([pid2genre[pid] for pid in topk_items]) #TODO Check with paper definitions
-    return len(diversity_items_tok)/len(topk_items)
-
-
-def novelty_at_k(topk_items, pid2popularity):
-    novelty_items_topk = [1 - pid2popularity[pid] for pid in topk_items]
-    return np.mean(novelty_items_topk)
 
 def exposure_pfairness(topk_items, pid2provider_popularity):
     exposure_providers_topk = [pid2provider_popularity[pid] for pid in topk_items]
--- pathlm/models/lm/decoding_constraints.py
+++ pathlm/models/lm/decoding_constraints.py
@@ -197,6 +197,10 @@ class PrefixConstrainedLogitsProcessorWordLevel(LogitsProcessor):
         return scores
 #'''
 
+"""
+https://dl.acm.org/doi/pdf/10.1145/3485447.3511937
+Constraint decoding strategy for PLM, it forces the model to generate alternatively entities and relations
+"""
 class PLMLogitsProcessorWordLevel(LogitsProcessor):
     def __init__(self, tokenized_kg, force_token_map, total_length, tokenizer, num_return_sequences,
                  id_to_uid_token_map, eos_token_ids, ent_mask, rel_mask, token_id_to_token,  **kwargs):
@@ -284,92 +288,3 @@ class PLMLogitsProcessorWordLevel(LogitsProcessor):
         '''
         #Set to min score the tokens which are not in force_tokens_map[cur_uid] at this position
         return scores
-    
-
-"""
-
-class TypifiedForceLastTokenLogitsProcessorWordLevel(LogitsProcessor):
-    def __init__(self, force_token_map, total_length, tokenizer, num_return_sequences, id_to_uid_token_map,
-                 eos_token_ids, **kwargs):
-        super().__init__(**kwargs)
-        self.force_token_map = force_token_map
-        self.total_length = total_length
-        self.tokenizer = tokenizer
-        self.used_tokens = defaultdict(list)
-        self.num_return_sequences = num_return_sequences
-        self.id_to_uid_token_map = id_to_uid_token_map
-        self.call_counter_to_process_finished_sequence = 0
-        self.eos_token_ids = eos_token_ids
-
-    def __call__(self, input_ids, scores):
-        # print(input_ids)
-        # print(input_ids.shape)
-        # print(input_ids.shape)
-        cur_len = input_ids.shape[-1]
-        # print(input_ids.shape, self.__decode(input_ids[0]))
-        # print(input_ids.shape, scores.shape)
-        # print(cur_len, input_ids[0])
-        '''
-        BLOCK = True
-        if cur_len == self.total_length-2 and not BLOCK:
-
-            #print(input_ids[0])
-            #print(scores.shape)
-            force_tokens = None
-            uid = None
-            user_tokens = None
-            UID_POS = 0
-            #print()
-            min_score = scores.min()
-            for idx in range(scores.shape[0]):
-                #if idx % self.num_return_sequences == 0:
-                #    #user_tokens = self.__decode([input_ids[idx,UID_POS]])
-                #    #user_tokens[-1][1:]
-                #     self.force_token_map[uid]   
-                uid = self.id_to_uid_token_map[input_ids[idx,UID_POS].item()] # user_tokens[-1][1:]
-                force_tokens = self.force_token_map[uid]                
-
-                #Compute min score in scores tensor
-                mask = np.isin(range(scores.shape[-1]), force_tokens)
-                used_mask = np.isin(range(scores.shape[-1]), self.used_tokens)
-                #print(used_mask.sum(), used_mask.shape)
-                mask = mask & ~used_mask  # Remove already used tokens from the mask
-                # Set to the smallest representable number
-                scores[idx, ~mask] = min_score#float('-Inf')
-        elif cur_len == self.total_length-1:
-            if not BLOCK:
-                num_tokens = scores.shape[1]
-                scores[:, [i for i in range(num_tokens) if i not in self.eos_token_ids]] = -float("inf")
-                for i in self.eos_token_ids:
-                    scores[:, i] = 0     
-
-            #for seq in input_ids:   
-            #    print(self.__decode(seq))
-            #print()
-        '''
-        return scores
-
-    def __decode(self, token_ids):
-        return self.tokenizer.convert_ids_to_tokens(token_ids)
-
-    def process_finished_sequence(self, input_ids):
-        # Remember the last token of the finished sequence
-        self.call_counter_to_process_finished_sequence += 1
-        self.used_tokens.append(input_ids[-1].item())
-
-class ForceTokenAtWordPositionLogitsProcessorBPE(LogitsProcessor):
-    def __init__(self, tokenizer, force_tokens, word_position, **kwargs):
-        super().__init__(**kwargs)
-        self.tokenizer = tokenizer
-        self.force_tokens = force_tokens
-        self.word_position = word_position
-
-    def __call__(self, input_ids, scores):
-        decoded_text = self.tokenizer.decode(input_ids[0])
-        word_count = decoded_text.count('<end_word>')
-        if word_count == self.word_position - 1:  # Subtract 1 because word count is 0-based
-            mask = np.isin(range(scores.shape[-1]), self.force_tokens)
-            scores[:, ~mask] = -float('Inf')
-        return scores
-        
-"""
