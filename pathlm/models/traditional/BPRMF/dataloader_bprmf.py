913e8ff563ece5882150437b76e5b60c3dce0a59
giacoballoccu <giacoballoccu@gmail.com>
2023-10-06 14:19:51 +0200

    Added BPRMF (TODO: Debug)

---
+++ pathlm/models/traditional/BPRMF/dataloader_bprmf.py
@@ -0,0 +1,18 @@
+from pathlm.models.traditional.data_loader_base import DataLoaderBase
+
+
+class DataLoaderBPRMF(DataLoaderBase):
+
+    def __init__(self, args, logging):
+        super().__init__(args, logging)
+        self.train_batch_size = args.train_batch_size
+        self.test_batch_size = args.test_batch_size
+        self.print_info(logging)
+
+
+    def print_info(self, logging):
+        logging.info('n_users:     %d' % self.n_users)
+        logging.info('n_items:     %d' % self.n_items)
+        logging.info('n_cf_train:  %d' % self.n_cf_train)
+        logging.info('n_cf_test:   %d' % self.n_cf_test)
+
---
+++ pathlm/models/traditional/BPRMF/main_bprmf.py
@@ -0,0 +1,201 @@
+import logging
+import os
+import sys
+import random
+from time import time
+
+import numpy as np
+import pandas as pd
+import torch
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+
+from pathlm.models.traditional.BPRMF.bprmf import BPRMF
+from pathlm.models.traditional.BPRMF.dataloader_bprmf import DataLoaderBPRMF
+from pathlm.models.traditional.BPRMF.parser_bprmf import parse_bprmf_args
+from pathlm.models.traditional.traditional_utils import early_stopping, save_model, load_model
+from pathlm.models.traditional.log_helper import logging_config, create_log_id
+
+
+def evaluate(model, dataloader, Ks, device):
+    test_batch_size = dataloader.test_batch_size
+    train_user_dict = dataloader.train_user_dict
+    test_user_dict = dataloader.test_user_dict
+
+    model.eval()
+
+    user_ids = list(test_user_dict.keys())
+    user_ids_batches = [user_ids[i: i + test_batch_size] for i in range(0, len(user_ids), test_batch_size)]
+    user_ids_batches = [torch.LongTensor(d) for d in user_ids_batches]
+
+    n_items = dataloader.n_items
+    item_ids = torch.arange(n_items, dtype=torch.long).to(device)
+
+    cf_scores = []
+    metric_names = ['precision', 'recall', 'ndcg']
+    metrics_dict = {k: {m: [] for m in metric_names} for k in Ks}
+
+    with tqdm(total=len(user_ids_batches), desc='Evaluating Iteration') as pbar:
+        for batch_user_ids in user_ids_batches:
+            batch_user_ids = batch_user_ids.to(device)
+
+            with torch.no_grad():
+                batch_scores = model(batch_user_ids, item_ids, is_train=False)       # (n_batch_users, n_items)
+
+            batch_scores = batch_scores.cpu()
+            batch_metrics = calc_metrics_at_k(batch_scores, train_user_dict, test_user_dict, batch_user_ids.cpu().numpy(), item_ids.cpu().numpy(), Ks)
+
+            cf_scores.append(batch_scores.numpy())
+            for k in Ks:
+                for m in metric_names:
+                    metrics_dict[k][m].append(batch_metrics[k][m])
+            pbar.update(1)
+
+    cf_scores = np.concatenate(cf_scores, axis=0)
+    for k in Ks:
+        for m in metric_names:
+            metrics_dict[k][m] = np.concatenate(metrics_dict[k][m]).mean()
+    return cf_scores, metrics_dict
+
+
+def train(args):
+    # seed
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+
+    log_save_id = create_log_id(args.save_dir)
+    logging_config(folder=args.save_dir, name='log{:d}'.format(log_save_id), no_console=False)
+    logging.info(args)
+
+    # GPU / CPU
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # load data
+    data = DataLoaderBPRMF(args, logging)
+    if args.use_pretrain == 1:
+        user_pre_embed = torch.tensor(data.user_pre_embed)
+        item_pre_embed = torch.tensor(data.item_pre_embed)
+    else:
+        user_pre_embed, item_pre_embed = None, None
+
+    # construct model & optimizer
+    model = BPRMF(args, data.n_users, data.n_items, user_pre_embed, item_pre_embed)
+    if args.use_pretrain == 2:
+        model = load_model(model, args.pretrain_model_path)
+
+    model.to(device)
+    logging.info(model)
+
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    # initialize metrics
+    best_epoch = -1
+    best_recall = 0
+
+    Ks = eval(args.Ks)
+    k_min = min(Ks)
+    k_max = max(Ks)
+
+    epoch_list = []
+    metrics_list = {k: {'precision': [], 'recall': [], 'ndcg': []} for k in Ks}
+
+    # train model
+    for epoch in range(1, args.n_epoch + 1):
+        model.train()
+
+        # train cf
+        time1 = time()
+        total_loss = 0
+        n_batch = data.n_cf_train // data.train_batch_size + 1
+
+        for iter in range(1, n_batch + 1):
+            time2 = time()
+            batch_user, batch_pos_item, batch_neg_item = data.generate_cf_batch(data.train_user_dict, data.train_batch_size)
+            batch_user = batch_user.to(device)
+            batch_pos_item = batch_pos_item.to(device)
+            batch_neg_item = batch_neg_item.to(device)
+            batch_loss = model(batch_user, batch_pos_item, batch_neg_item, is_train=True)
+
+            if np.isnan(batch_loss.cpu().detach().numpy()):
+                logging.info('ERROR: Epoch {:04d} Iter {:04d} / {:04d} Loss is nan.'.format(epoch, iter, n_batch))
+                sys.exit()
+
+            batch_loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            total_loss += batch_loss.item()
+
+            if (iter % args.print_every) == 0:
+                logging.info('CF Training: Epoch {:04d} Iter {:04d} / {:04d} | Time {:.1f}s | Iter Loss {:.4f} | Iter Mean Loss {:.4f}'.format(epoch, iter, n_batch, time() - time2, batch_loss.item(), total_loss / iter))
+        logging.info('CF Training: Epoch {:04d} Total Iter {:04d} | Total Time {:.1f}s | Iter Mean Loss {:.4f}'.format(epoch, n_batch, time() - time1, total_loss / n_batch))
+
+        # evaluate cf
+        if (epoch % args.evaluate_every) == 0 or epoch == args.n_epoch:
+            time3 = time()
+            _, metrics_dict = evaluate(model, data, Ks, device)
+            logging.info('CF Evaluation: Epoch {:04d} | Total Time {:.1f}s | Precision [{:.4f}, {:.4f}], Recall [{:.4f}, {:.4f}], NDCG [{:.4f}, {:.4f}]'.format(
+                epoch, time() - time3, metrics_dict[k_min]['precision'], metrics_dict[k_max]['precision'], metrics_dict[k_min]['recall'], metrics_dict[k_max]['recall'], metrics_dict[k_min]['ndcg'], metrics_dict[k_max]['ndcg']))
+
+            epoch_list.append(epoch)
+            for k in Ks:
+                for m in ['precision', 'recall', 'ndcg']:
+                    metrics_list[k][m].append(metrics_dict[k][m])
+            best_recall, should_stop = early_stopping(metrics_list[k_min]['recall'], args.stopping_steps)
+
+            if should_stop:
+                break
+
+            if metrics_list[k_min]['recall'].index(best_recall) == len(epoch_list) - 1:
+                save_model(model, args.save_dir, epoch, best_epoch)
+                logging.info('Save model on epoch {:04d}!'.format(epoch))
+                best_epoch = epoch
+
+    # save metrics
+    metrics_df = [epoch_list]
+    metrics_cols = ['epoch_idx']
+    for k in Ks:
+        for m in ['precision', 'recall', 'ndcg']:
+            metrics_df.append(metrics_list[k][m])
+            metrics_cols.append('{}@{}'.format(m, k))
+    metrics_df = pd.DataFrame(metrics_df).transpose()
+    metrics_df.columns = metrics_cols
+    metrics_df.to_csv(args.save_dir + '/metrics.tsv', sep='\t', index=False)
+
+    # print best metrics
+    best_metrics = metrics_df.loc[metrics_df['epoch_idx'] == best_epoch].iloc[0].to_dict()
+    logging.info('Best CF Evaluation: Epoch {:04d} | Precision [{:.4f}, {:.4f}], Recall [{:.4f}, {:.4f}], NDCG [{:.4f}, {:.4f}]'.format(
+        int(best_metrics['epoch_idx']), best_metrics['precision@{}'.format(k_min)], best_metrics['precision@{}'.format(k_max)], best_metrics['recall@{}'.format(k_min)], best_metrics['recall@{}'.format(k_max)], best_metrics['ndcg@{}'.format(k_min)], best_metrics['ndcg@{}'.format(k_max)]))
+
+
+def predict(args):
+    # GPU / CPU
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # load data
+    data = DataLoaderBPRMF(args, logging)
+
+    # load model
+    model = BPRMF(args, data.n_users, data.n_items)
+    model = load_model(model, args.pretrain_model_path)
+    model.to(device)
+
+    # predict
+    Ks = eval(args.Ks)
+    k_min = min(Ks)
+    k_max = max(Ks)
+
+    cf_scores, metrics_dict = evaluate(model, data, Ks, device)
+    np.save(args.save_dir + 'cf_scores.npy', cf_scores)
+    print('CF Evaluation: Precision [{:.4f}, {:.4f}], Recall [{:.4f}, {:.4f}], NDCG [{:.4f}, {:.4f}]'.format(
+        metrics_dict[k_min]['precision'], metrics_dict[k_max]['precision'], metrics_dict[k_min]['recall'], metrics_dict[k_max]['recall'], metrics_dict[k_min]['ndcg'], metrics_dict[k_max]['ndcg']))
+
+
+
+if __name__ == '__main__':
+    args = parse_bprmf_args()
+    train(args)
+    # predict(args)
+
