e50b43b255adc93fe60af4528599c5a153093050
giacoballoccu <giacoballoccu@gmail.com>
2024-03-19 10:37:03 +0100

    Code cleaning and parameters reset

--- create_dataset.sh
+++ create_dataset.sh
@@ -5,15 +5,10 @@ NPROC=$4
 
 
 ROOT_DIR='./'
-DATA_DIR='data_v2'
+DATA_DIR='data'
 TASK='end-to-end'
 
 LOGDIR=$(echo "dataset_${DATASET}__hops_${HOPS}__npaths_${NPATHS}")
-
-#pip install ../.. && python3 main_nfm.py --dataset $DATASET --max_n_paths $NPATHS --max_hop $HOPS --collaborative TRUE --nproc $NPROC
-#find ./statistics/$DATASET/$LOGDIR -name '*.txt' -exec cat {} \; >> concatenated_rw_file.txt
-#python3 prune_dataset.py --filepath concatenated_rw_file.txt
-#mv concatenated_rw_file.txt ../data/$DATASET/paths_random_walk/paths_${TASK}_${NPATHS}_${HOPS}.txt
 
 pip install . && python3 pathlm/sampling/main.py --root_dir $ROOT_DIR --data_dir $DATA_DIR --dataset $DATASET --max_n_paths $NPATHS --max_hop $HOPS --collaborative TRUE --nproc $NPROC
 find $ROOT_DIR/$DATA_DIR/sampled/$DATASET/$LOGDIR -name '*.txt' -exec cat {} \; >> concatenated_rw_file.txt
