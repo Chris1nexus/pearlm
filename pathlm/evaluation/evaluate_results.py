b1b014a8abde532813158d9d0a861ff90168fc68
giacoballoccu <giacoballoccu@gmail.com>
2024-03-22 11:53:23 +0100

    Bugfix where PLM evaluation couldn't start

--- pathlm/evaluation/evaluate_results.py
+++ pathlm/evaluation/evaluate_results.py
@@ -16,6 +16,8 @@ if __name__ == '__main__':
     parser.add_argument('--decoding_strategy', default='gcd', type=str, help='')
     args = parser.parse_args()
 
+    if 'plm-rec' in args.model:
+        args.model = f'end-to-end@{args.dataset}@{args.model}@{args.sample_size}@{args.n_hop}@{args.k}@'
     if args.model in ['distilgpt2', 'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl']:
         args.model = f'end-to-end@{args.dataset}@{args.model}@{args.sample_size}@{args.n_hop}@{args.decoding_strategy}'
     result_dir =  get_result_dir(args.dataset, args.model)
